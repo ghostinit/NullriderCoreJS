1b6bf541e2c863e215ee7e1dd84da76dae82af23
Adam Lyon <adam@nullrider.com>
2025-05-24 09:26:24 -0600

    Removed test code from logger.js

--- logger.js
+++ logger.js
@@ -40,12 +40,3 @@ class Logger {
 
 module.exports = Logger;
 
-// File: test.js
-const { Logger } = require('./index');
-
-const logger = new Logger('TEST');
-
-logger.debug('This is a debug message');
-logger.info('This is an info message');
-logger.warn('This is a warning');
-logger.error('This is an error');
